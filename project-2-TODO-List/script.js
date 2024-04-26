5f4c8d521acaa1738410d3c107c667fdf054bfeb
Tejas Kadam <129816845+Tejaskadam2626@users.noreply.github.com>
2024-04-26 10:02:51 +0530

    Update script.js

--- project-2-TODO-List/script.js
+++ project-2-TODO-List/script.js
@@ -1,1 +1,29 @@
+const inputBox=document.querySelector('#input-box');
+const liContainer=document.querySelector('#list-container');
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        liContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+   
+}
+liContainer.addEventListener("click",function(e){
+    if(e.target.tagName=="LI"){
+        e.target.classList.toggle("checked");
+       
+    }
+    else if(e.target.tagName=="SPAN"){
+        e.target.parentElement.remove();
+       
+    }
+},false);
 
